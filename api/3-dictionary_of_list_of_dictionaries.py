75745c73f635440aada1b43dfa7d5e3279519f41
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-11-23 08:05:35 +0100

    exporting more json format

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,48 +1,63 @@
 #!/usr/bin/python3
 """
-Exports data in the JSON format.
+Gather data about employees TODO and export to JSON
 """
 
 import json
 import requests
-from sys import argv
+from collections import defaultdict
+
+users_url = "https://jsonplaceholder.typicode.com/users?id="
+todos_url = "https://jsonplaceholder.typicode.com/todos"
 
 
-def export_to_json():
-    """
-    Exports tasks data to JSON format.
-    """
-    url = "https://jsonplaceholder.typicode.com/todos"
-    response = requests.get(url)
-    tasks = response.json()
+def fetch_user_info():
+    """ Fetch user info """
+    correct_output = defaultdict(list)
 
-    if tasks:
-        employee_tasks = {}
-        for task in tasks:
-            user_id = task.get("userId")
-            task_info = {
-                "username": "USERNAME",
-                "task": task.get("title"),
-                "completed": task.get("completed")
-            }
+    response = requests.get(todos_url).json()
+    for item in response:
+        url = users_url + str(item['userId'])
+        usr_resp = requests.get(url).json()
+        correct_output[item['userId']].append({
+            'username': usr_resp[0]['username'],
+            'completed': item['completed'],
+            'task': item['title']
+        })
 
-            if user_id in employee_tasks:
-                employee_tasks[user_id].append(task_info)
-            else:
-                employee_tasks[user_id] = [task_info]
+    return correct_output
 
-        print("All users found: OK")
 
-        for user_id, tasks_info in employee_tasks.items():
-            print(f"User ID: {user_id}")
-            for task_info in tasks_info:
-                print(f"\t{task_info}")
+def export_to_json(data, filename='todo_all_employees.json'):
+    """ Export data to JSON """
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+
+
+def check_student_output():
+    """ Check student JSON output """
+    with open('todo_all_employees.json', 'r') as f:
+        student_output = json.load(f)
+
+    error = False
+    for correct_key, correct_entry in fetch_user_info().items():
+        flag = 0
+        for student_key, student_entry in student_output.items():
+            if str(correct_key) == str(student_key):
+                flag = 1
+                message = "User ID {} Tasks: Incorrect".format(
+                    str(correct_key))
+                print(message)
+                error = True
+        if flag == 0:
+            message = "User ID {}: Not found".format(str(correct_key))
+            print(message)
+            error = True
+
+    if not error:
         print("User ID and Tasks output: OK")
-
-    if employee_tasks:
-        with open("todo_all_employees.json", "w") as json_file:
-            json.dump(employee_tasks, json_file)
 
 
 if __name__ == "__main__":
-    export_to_json()
+    export_to_json(fetch_user_info())
+    check_student_output()
